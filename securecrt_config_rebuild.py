4f2633dcab5693394c90735ef9b27f68cb2bec41
Pavlov Aleksey <a.pavlov@projectsupport.ru>
2022-07-06 12:23:02 +0300

    Refactoring securecrt_config_rebuild.py

--- securecrt_config_rebuild.py
+++ securecrt_config_rebuild.py
@@ -1,35 +1,36 @@
 import re
 import os
 
-path_f = [] 
-for d, dirs, files in os.walk('.'):
-    for file in files:
-        if file.endswith('.ini') and file != '__FolderData__.ini' and f[-3:] != '.py':
-            path = os.path.join(d,file) # формирование адреса 
-            path_f.append(path) # добавление адреса в список 
-            
-def replace_in_file(file, username = None, password = None):
+
+def config_search():
+    configs_list = []
+    for root, _, files in os.walk('.'):
+        for file in files:
+            if file.endswith('.ini') and file != '__FolderData__.ini':
+                path = os.path.join(root, file)  # формирование адреса
+                configs_list.append(path)  # добавление адреса в список
+    return configs_list
+
+
+def replace_in_file(config_file, username_str: str = None, password_str: str = None) -> None:
     try:
-        with open(file) as file_in:
-            text = file_in.read()
-            text = re.sub(r'S:\"Username\"\S+', 'S:\"Username\"={}'.format(username), text)
-            text = re.sub(r'S:\"Password V2\"\S+', 'S:\"Password V2\"={}'.format(password), text)
+        with open(config_file) as config:
+            text = config.read()
+            text = re.sub(r'S:\"Username\"\S+', 'S:\"Username\"={}'.format(username_str), text)
+            text = re.sub(r'S:\"Password V2\"\S+', 'S:\"Password V2\"={}'.format(password_str), text)
             text = re.sub(r'S:\"Color Scheme\"\S+', 'S:\"Color Scheme\"=Birds of Paradise', text)
             text = re.sub(r'S:\"Keyword Set\"\S+', 'S:\"Keyword Set\"=<None>', text)
-        with open(file, "w") as file_out:
-            file_out.write(text)
-    except:
-        pass
+        with open(config_file, "w") as new_config:
+            new_config.write(text)
+    except OSError as error:
+        print(error)
 
 
 if __name__ == "__main__":
     username = ''
     password = ''
 
-    i = len(path_f) - 1
-    while i >= 0:
-        file = path_f[i]
-        replace_in_file(file)
+    for config in config_search():
+        replace_in_file(config)
+
         #os.rename(file, file[:-4]+"_"+ re.search(r'[а-яА-ЯёЁ]+', file).group(0) + '.ini') # МФЦ только. переименует файл конфигурации, добавит имя папки в конце имени файла.
-        print(i, file)
-        i = i - 1
