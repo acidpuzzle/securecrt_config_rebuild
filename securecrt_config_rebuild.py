aaffd80c20b95ce661989b2098ffe2e9739f9e63
Pavlov Aleksey <a.pavlov@projectsupport.ru>
2022-07-06 12:54:03 +0300

    refact. securecrt_config_rebuild.py

--- securecrt_config_rebuild.py
+++ securecrt_config_rebuild.py
@@ -1,3 +1,8 @@
+"""
+Скрипт проходит по вложенным каталогам и меняет логин и пароль в конфигурациях securecrt
+Конфиги обычно лежат тут: %USERPROFILE%\AppData\Roaming\VanDyke\Config\
+"""
+
 import re
 import os
 
@@ -14,23 +19,23 @@ def config_search():
 
 def replace_in_file(config_file, username_str: str = None, password_str: str = None) -> None:
     try:
-        with open(config_file) as config:
-            text = config.read()
-            text = re.sub(r'S:\"Username\"\S+', 'S:\"Username\"={}'.format(username_str), text)
-            text = re.sub(r'S:\"Password V2\"\S+', 'S:\"Password V2\"={}'.format(password_str), text)
-            text = re.sub(r'S:\"Color Scheme\"\S+', 'S:\"Color Scheme\"=Birds of Paradise', text)
-            text = re.sub(r'S:\"Keyword Set\"\S+', 'S:\"Keyword Set\"=<None>', text)
-        with open(config_file, "w") as new_config:
+        with open(config_file, encoding='UTF8') as old_config:
+            text = old_config.read()
+
+        text = re.sub(r'S:\"Username\"\S+', 'S:\"Username\"={}'.format(username_str), text)
+        text = re.sub(r'S:\"Password V2\"\S+', 'S:\"Password V2\"={}'.format(password_str), text)
+        text = re.sub(r'S:\"Color Scheme\"\S+', 'S:\"Color Scheme\"=Birds of Paradise', text)
+        text = re.sub(r'S:\"Keyword Set\"\S+', 'S:\"Keyword Set\"=<None>', text)
+
+        with open(config_file, "w", encoding='UTF8') as new_config:
             new_config.write(text)
     except OSError as error:
         print(error)
 
 
 if __name__ == "__main__":
-    username = ''
-    password = ''
+    username = ''  # тут прописать логин.
+    password = ''  # тут прописать ХЭШ пароля, можно взять из уже сохраненного конфига с правильным паролем.
 
     for config in config_search():
-        replace_in_file(config)
-
-        #os.rename(file, file[:-4]+"_"+ re.search(r'[а-яА-ЯёЁ]+', file).group(0) + '.ini') # МФЦ только. переименует файл конфигурации, добавит имя папки в конце имени файла.
+        replace_in_file(config, username_str=username, password_str=password)
